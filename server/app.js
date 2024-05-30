4fd26b5735b90e2a7e13c151ac0731660898617e
Brandon Mitchell <brandonmitchell217@gmail.com>
2024-05-29 22:17:42 -0400

    [feat]: find player by id

--- server/app.js
+++ server/app.js
@@ -31,6 +31,22 @@ app.get("/api/players", async (req, res) => {
   }
 });
 
+app.get("/api/players/:id", async (req, res) => {
+  try {
+    const playerId = req.params.id;
+    const player = await Player.findOne({ PlayerId: playerId });
+
+    if (!player) {
+      return res.status(404).json({ message: "Player not found" });
+    }
+
+    res.status(200).json(player);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 app.get("/api/players/qb", async (req, res) => {
   try {
     const qb = await QB.find({});
