d1e51cb005c833f02328ac5f254468ba2195ee1e
Brandon Mitchell <brandonmitchell217@gmail.com>
2024-05-30 20:04:28 -0400

    update

--- server/app.js
+++ server/app.js
@@ -87,17 +87,6 @@ app.get("/api/players/te", async (req, res) => {
   }
 });
 
-// app.get("/products/:id", async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const product = await Product.findById(id);
-//     res.status(200).json(product);
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
 mongoose
   .connect(process.env.MONGODB_URI || "")
   .then(() => {
